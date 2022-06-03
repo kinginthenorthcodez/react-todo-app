e7748237a317107b4e7663ec4893107425bd261d
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-06-03 17:49:39 +0530

    Implement edit

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -68,6 +68,17 @@ class TodoContainer extends React.Component {
     });
   }
 
+  setUpdate = (updatedTitle, id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
+        }
+        return todo;
+      }),
+    }));
+  }
+
   render() {
     const { todos } = this.state;
     return (
@@ -79,6 +90,7 @@ class TodoContainer extends React.Component {
             todos={todos}
             handleChangeProps={this.handleChange}
             deleteTodoProps={this.delTodo}
+            setUpdate={this.setUpdate}
           />
         </div>
       </div>
