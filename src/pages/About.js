5762e30a51fd98208c5c45874a2c71eb9ff5c3ec
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-06-04 02:49:50 +0530

    update Files

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
 import React from 'react';
 
-const About = () => <div>hello from about page</div>;
+const About = () => <div>hello from about page, this is routes lesson</div>;
 export default About;
